e2183b26caf3eaa287b4761f0a19ca6b31ae6972
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2024-06-12 16:41:55 -0300

    o que é um array

---
+++ php-basico/array.php
@@ -0,0 +1,18 @@
+<?php
+    /*/
+        o que é um array? um array é uma variavel onde podemos armazenar mais de um valor.
+    */
+
+    // formas de declarar um array
+
+    $nome = array('Wilson', 'João', 'Maria', 'José', 'Ana'); // criando um array de strings
+    echo $nome[0]; // acessando o primeiro valor do array 
+
+    $nome2[] = 'Wilson';
+    echo $nome2[0]; // acessando o primeiro valor do array
+
+    $numbers = [1, 2, 3, 4, 5]; // criando um array de inteiros
+    echo $numbers[1]; // acessando o segundo valor do array
+
+    $arrayAleatorio = ['Wilson', 20, true, 1.8] // criando um array com valores de tipos diferentes
+?>
