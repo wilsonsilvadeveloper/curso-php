b506994b921cfc1899ecff1d81a6208e2e089488
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2024-06-12 16:09:32 -0300

    o que é uma variavel?

---
+++ php-basico/print.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Minha pagina de teste</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <p>
+            <?php
+                 echo "Olá mundo :)"
+            ?>
+        </p>
+    </body>
+</html>
---
+++ php-basico/variaveis.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*/
+        uma variavel é um espaço na memória do computador que armazena um valor, ao 
+        criar uma variavel, estamos reservando um espaço na memória para armazenar um valor para
+        posteriomente ser acessado e manipulado.
+    */
+
+    echo '<pre>';
+    // acessando variáveis já definidas pelo PHP
+    print_r($_SERVER);
+    echo '</pre>';
+
+    // criando novas variáveis 
+    $nome = 'Wilson'; // variavel do tipo string (ou seja, uma seguencia de caracteres)
+    echo 'Meu nome é <br/> ', $nome;
+
+    $idade = 20; // variavel do tipo inteiro (ou seja, um número inteiro)
+    echo '<br/> Eu tenho ' .$idade .' anos';
+
+    $bool = true; // variavel do tipo booleano (ou seja, um valor verdadeiro ou falso)
+    echo '<br/> A variável $bool é do tipo booleano? ' . $bool;
+
+    $altura = 1.88; // variavel do tipo double (ou seja, um número com casas decimais)
+    echo '<br/> eu tenho ' . $altura . ' de altura';
+
+?>
