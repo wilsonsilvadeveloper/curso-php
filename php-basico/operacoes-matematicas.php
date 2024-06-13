d1a1bfc0df9c8e5a48ca1a9f6e3297ad369147c7
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2024-06-13 15:18:08 -0300

    operações matematicas com php

---
+++ php-basico/operacoes-matematicas.php
@@ -0,0 +1,23 @@
+<?php
+    $adicao = 5 + 5;
+    echo $adicao; // 10
+
+    $subtracao = 10 - 5; // 5
+    echo "<br/> $subtracao";
+
+    $adicaoDeVariaveis = $adicao + $subtracao; // 15
+    echo "<br/> $adicaoDeVariaveis";
+
+    $multiplicacao = 5 * 5; // 25
+    echo "<br/> $multiplicacao";
+
+    $divisao = 10/2; // 5
+    echo "<br/> $divisao";
+
+    $expressao = (10 + 20 - 4) * 3 / 2; // 39
+    echo "<br/> $expressao";
+
+    $resto = 4 % 2; // 0
+    echo "<br/> $resto";
+
+?>
