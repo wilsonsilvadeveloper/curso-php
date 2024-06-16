4d94edf63617e89d881a565245a886f1b6fa01f8
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2024-06-15 22:15:51 -0300

    add new comentario

--- php-intermediario/include-e-date.php
+++ php-intermediario/include-e-date.php
@@ -4,5 +4,9 @@
     $data = date('d/m/Y H:i:s', time()+(60*10)); // pega a data atual e soma 10 minutos
     echo $data . '<br/>'; // exibe a data atual + 10 minutos
 
-    include('../php-basico/print.php');
+
+    /*/
+        o include() é uma função que inclui e executa o código de um arquivo externo.
+    */
+    include('../php-basico/print.php'); // inclui o arquivo 'print.php' que está na pasta 'php-basico'
 ?>
